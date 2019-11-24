523759da5ed890ae7115cd52d19e0ea517cebf39
Robert <vanphuong.nguyen@my.jcu.edu.au>
2019-11-24 18:44:47 +0800

    Finish Add Movie Feature

--- assignment1.py
+++ assignment1.py
@@ -8,8 +8,8 @@ GitHub URL: https://github.com/robert09021998
 def main():
     # Load file and get data
     print("Movies To Watch 1.0 - by Nguyen Van Phuong")
-    in_file = open('movies.csv', 'a+')
-    data = load_data(in_file)
+    in_file = open('movies.csv', 'r+')
+    data = in_file.readlines()
     print('{} movies loaded'.format(len(data)))
 
     # Menu lopper
@@ -29,28 +29,29 @@ def main():
         elif choice == 'W':
             watch_movie(data)
         elif choice == 'Q':
-            save_data(data)
+            # Join the data from list
+            join_data = ''.join(data)
+            # Seek to the beginning of the file
+            in_file.seek(0)
+            in_file.write(join_data)
             print('{} movies saved to movies.csv'.format(len(data)))
             break
         else:
             print('Invalid menu choice')
-        
     
+    # Close file
+    in_file.close()
 
-
-# Laod file
-def load_data(in_file):
-    in_file.seek(0)
-    data = in_file.readlines()
-    return data
-
-# Show list movies
+# ===============
+# SHOW MOVIE LIST
+# ===============
 def list_movies(data):
     # Find the max length of titles
     longest_title = find_longest_title(data)
     # Print out list of movie based on raw data
     show_movie(data, longest_title)
-        
+
+    
 # Find longest title
 def find_longest_title(data):
     longest_title = 0
@@ -67,10 +68,8 @@ def show_movie(raw_data, longest_title):
     for movie in raw_data:
         # Split data by ','
         splited_data = movie.split(',')
-        title_max_length = 0
         mark = '*' if 'u' in splited_data[3] else ''
         title = splited_data[0]
-        title_max_length = len(title) if len(title) > title_max_length else title_max_length
         year = splited_data[1]
         genre = splited_data[2]
         print('{0:<2} {1:<{2}} - {3:<4} ({4})'.format(mark, title, longest_title ,year, genre))
@@ -81,19 +80,68 @@ def show_movie(raw_data, longest_title):
     
     unwatch_count = len(raw_data) - watch_count
     print('{} movies watched, {} movies still to watch'.format(watch_count, unwatch_count))
+        
 
-# Add new movie
+# =========
+# ADD MOVIE
+# =========
 def add_movies(data):
-    return
+    # User input
+    def functionA():
+        return
+    title = text_handler('Title: ')
+    year = year_handler()
+    genre = text_handler('Category: ')
 
-# Watch a movie
+    
+    # Save to data
+    data_to_save = '\n{},{},{},u'.format(title, year, genre)
+    data.append(data_to_save)
+    # join_data = ''.join(data)
+    # print(join_data)
+
+# Text handler
+def text_handler(input_name):
+    user_input = input(input_name)
+    while not user_input.strip():
+        print('Input cannot be blank')
+        user_input = input(input_name)
+
+    return user_input
+    
+# Year handler
+def year_handler():
+    while True:
+        try:
+            year_input = int(input('Year: '))
+            if year_input <= 0:
+                raise Exception('YearError')
+            break
+        except ValueError:
+            print('Invalid input, enter a valid number')
+        except Exception:
+            print('Your number must be more than 0')
+    
+    return year_input
+
+            
+    
+
+
+# ===========
+# WATCH MOVIE
+# ===========
 def watch_movie(data):
     return
 
-# Save movies:
+# ==========
+# SAVE MOVIE
+# ==========
 def save_data(data):
     return
 
-# Run main funciton
+# ========
+# RUN MAIN
+# ========
 if __name__ == '__main__':
     main()
